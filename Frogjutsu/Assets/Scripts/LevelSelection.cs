05fdc1bed89abeffed70b3ca6f73f06b719d0114
Faris Khattak <99377493+fariskhattak@users.noreply.github.com>
2024-03-22 11:50:21 -0500

    fixed semicolon errors levelselection.cs

--- Frogjutsu/Assets/Scripts/LevelSelection.cs
+++ Frogjutsu/Assets/Scripts/LevelSelection.cs
@@ -9,22 +9,28 @@ public class LevelSelection : MonoBehaviour
     [SerializeField] private bool unlocked; //default value is false
     public Image unlockImage;
 
-    public void Update(){ //TODO move this method later
+    public void Update()
+    { //TODO move this method later
         UpdateLevelImage();
     }
-    
-    public void UpdateLevelImage(){
-        if (!unlocked){ //MARKER if unlock is false means this level is locked
+
+    public void UpdateLevelImage()
+    {
+        if (!unlocked)
+        { //MARKER if unlock is false means this level is locked
             unlockImage.gameObject.SetActive(true);
         }
-        else { //if unlock is true means this level can play
-            unlockImage.gameObject.SetActive(false)
+        else
+        { //if unlock is true means this level can play
+            unlockImage.gameObject.SetActive(false);
         }
     }
 
-    public void PressSelection(string _LevelName) { //when press level, move to the corresponding scene
-        if(unlocked) {
-            SceneManager.LoadScene(_LevelName)
+    public void PressSelection(string _LevelName)
+    { //when press level, move to the corresponding scene
+        if (unlocked)
+        {
+            SceneManager.LoadScene(_LevelName);
         }
     }
 }
