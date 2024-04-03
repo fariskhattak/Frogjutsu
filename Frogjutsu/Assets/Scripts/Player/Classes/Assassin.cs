c8de20713ea45289730c66481f6323ea5f5a096a
Faris Khattak <99377493+fariskhattak@users.noreply.github.com>
2024-04-03 17:15:34 -0500

    assassin attack

--- Frogjutsu/Assets/Scripts/Player/Classes/Assassin.cs
+++ Frogjutsu/Assets/Scripts/Player/Classes/Assassin.cs
@@ -5,8 +5,35 @@ using UnityEngine;
 public class Assassin : Player
 {
     public float additionalMoveSpeed = 3f;
+    public GameObject attackPoint;
+    public float attackRadius;
     public override void Run(float dirX)
     {
         rb.velocity = new Vector2(dirX * (moveSpeed + additionalMoveSpeed), rb.velocity.y);
+    }
+
+    // Used for melee attacks
+    public void MeleeAttack()
+    {
+        if (attackPoint != null)
+        {
+            Collider2D[] enemy = Physics2D.OverlapCircleAll(attackPoint.transform.position, attackRadius, enemies);
+
+            foreach (Collider2D enemyGameObject in enemy)
+            {
+                Debug.Log("Hit Enemy");
+                enemyGameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (attackPoint != null)
+        {
+            Gizmos.DrawWireSphere(attackPoint.transform.position, attackRadius);
+        }
+
     }
 }
