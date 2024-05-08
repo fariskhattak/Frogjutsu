c88dc835c24dd68f1cdc0f195bc08c51e865df90
Milan Nguyen <m.nguyen2035@gmail.com>
2024-05-08 17:39:42 -0500

    boss created

---
+++ Frogjutsu/Assets/Scripts/Enemies/Boss/BossEnemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 100;
+    }
+}
